9aca48000ff89c47b3ba83a6843946971927220c
raphaelabaldi <raphaelabaldi12345@gmail.com>
2024-06-13 16:14:28 -0300

    add food, collision and game over menu

--- js/script.js
+++ js/script.js
@@ -1,12 +1,51 @@
 const canvas = document.querySelector('canvas');
-const ctx = canvas.getContext("2d");
+const score = document.querySelector('.score--value');
+const finalScore = document.querySelector('.final-score > span');
+const menu = document.querySelector('.menu-screen');
+const buttonPlay = document.querySelector('.btn-play');
 
+const ctx = canvas.getContext("2d");
+const audio = new Audio("../assets/audio.mp3");
+
+const initialPosition = { x: 270, y: 240 }
 const snakeBoxSize = 30;
-const snake = [
-    { x:270, y:240 }
-];
+let snake = [initialPosition];
+
+const incrementScore = () => {
+    score.innerText = +score.innerText + 10;
+}
+
+const randomNumber = ( min, max ) => {
+    return Math.round(Math.random() * (max - min) + min);
+}
+
+const randomPosition = () => {
+    const number = randomNumber(0, canvas.width - snakeBoxSize);
+    return Math.round(number / 30) * 30;
+}
+
+const randomColor = () => {
+    const red = randomNumber(0, 255);
+    const green = randomNumber(0, 255);
+    const blue = randomNumber(0, 255)
+
+    return `rgb(${red}, ${green}, ${blue})`
+}
+
+const food = { x: randomPosition(), y: randomPosition(), color: randomColor()};
 
 let direction, loopId;
+
+const drawFood = () => {
+    const {x, y, color} = food;
+
+    ctx.shadowColor = color;
+    ctx.shadowBlur = 6;
+    ctx.fillStyle = color;
+    ctx.fillRect(x, y, snakeBoxSize, snakeBoxSize);
+    // zerar o blur após desenhar os retangulos, se não ele aplica blur no grid e na cobra também.
+    ctx.shadowBlur = 0;
+}
 
 const drawSnake = () => {
     ctx.fillStyle = "#ddd";
@@ -41,12 +80,7 @@ const moveSnake = () => {
         case "down":
             snake.push({ x: snakeHead.x, y: snakeHead.y + snakeBoxSize });
             break;
-
-        default:
-            console.log("Error: Invalid direction");
-            break;
     }
-
     snake.shift()
 }
 
@@ -67,15 +101,64 @@ const drawGrid = () => {
     }
 }
 
+const checkEat = () => {
+    const snakeHead = snake[snake.length -1];
+
+    if (snakeHead.x == food.x && snakeHead.y == food.y) {
+        incrementScore();
+        snake.push(snakeHead);
+        audio.play();
+
+        let x = randomPosition();
+        let y = randomPosition();
+
+        // para que a comida não seja gerada dentro da cobrinha, é verificado a posição. Caso a posição da comida e da cobra seja igual, ele gera uma nova posição dentro do canvas
+        while(snake.find((position) => position.x == x && position.y == y)) {
+            x = randomPosition();
+            y = randomPosition();
+        }
+
+        food.x = x;
+        food.y = y;
+        food.color = randomColor();
+    };
+}
+
+const checkCollision = () => {
+    const snakeHead = snake[snake.length - 1]
+    const canvasLimit = canvas.width - snakeBoxSize
+    const neckIndex = snake.length - 2
+
+    const wallCollision = snakeHead.x < 0 || snakeHead.x > canvasLimit || snakeHead.y < 0 || snakeHead.y > canvasLimit;
+    const selfCollision = snake.find((position, index) => {
+        return index < neckIndex && position.x == snakeHead.x && position.y == snakeHead.y
+    })
+
+    if (wallCollision || selfCollision) {
+        gameOver();
+    };
+}
+
+const gameOver = () => {
+    direction = undefined;
+    
+    menu.style.display = "flex";
+    finalScore.innerText = score.innerText;
+    canvas.style.filter = "blur(2px)";
+}
+
 const gameLoop = () => {
     clearInterval(loopId);
 
     ctx.clearRect(0, 0, 600, 600);
     drawGrid();
-    drawSnake();
+    drawFood();
     moveSnake();
+    drawSnake();
+    checkEat();
+    checkCollision();
 
-    loopId = setTimeout(() => { gameLoop() }, 500);
+    loopId = setTimeout(() => { gameLoop() }, 300);
 }
 
 gameLoop();
@@ -96,4 +179,12 @@ document.addEventListener("keydown", ({key}) => {
    if (key === "ArrowUp" && direction != "down") {
     direction = "up"
    }
+})
+
+buttonPlay.addEventListener("click", () => {
+    score.innerText = "00";
+    menu.style.display = "none";
+    canvas.style.filter = "none";
+
+    snake = [initialPosition]
 })
